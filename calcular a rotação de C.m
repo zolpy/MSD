7bfe134b6ae83a4c668c5ae70707ff428355b9ec
Brandão <localcidadao@bol.com.br>
2020-03-30 12:42:07 -0300

    Create calcular a rotação de C.m

---
+++ calcular a rotação de C.m
@@ -0,0 +1,8 @@
+dA=input('Digite o valor do diametro A');
+dB=input('Digite o valor do diametro B');
+dC=input('Digite o valor do diametro C');
+dD=input('Digite o valor do diametro D');
+nA=600;
+nC = (nA*dA*dD)/(dB*dC);
+disp('A rotação em C é:', nC);
+
